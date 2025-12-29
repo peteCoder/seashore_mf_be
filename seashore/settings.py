82b50d5c9ed416de54999ff45969b46e10cf9813
ghostpete <ghostcoderme@gmail.com>
2025-12-29 09:37:05 +0100

    collected staticfiles

--- seashore/settings.py
+++ seashore/settings.py
@@ -142,6 +142,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:3001",
+
 ]
 
 # ✅ CRITICAL: Allow credentials (cookies)
@@ -218,6 +219,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:8000',      # ← ADD: For admin
     'http://127.0.0.1:8000',      # ← ADD: For admin
+    'seashore-mf-be.vercel.app',
 ]
 
 # Content Security Policy
